5c7ddb368d09467f8f85d166aeab51a81d0a07e7
Jaime Molina <js.molina13@uniandes.edu.co>
2017-10-06 13:34:42 -0500

    working process

--- Week5/ex4/randInitializeWeights.m
+++ Week5/ex4/randInitializeWeights.m
@@ -19,7 +19,7 @@ function W = randInitializeWeights(L_in, L_out)
   % Note: The first column of W corresponds to the parameters for the bias unit
   %
   epsilon_init = 0.12;
-  W = rand(L_out, 1 + L_in) * 2 * epsilon_init − epsilon_init;
+  W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 
 
 % =========================================================================
